02e0618a85809b657a6291a2cff6f19009b651d1
Sei <ssantelicesg@gmail.com>
2021-05-20 13:27:35 -0400

    make it herokubetter

--- src/index.js
+++ src/index.js
@@ -13,6 +13,15 @@ app.listen(PORT, () => {
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Configurar cabeceras y cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    // res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    // res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 console.log(PORT);
 console.log(MONGODB_URL);
 
